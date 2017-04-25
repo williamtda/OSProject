e9d9408fd1c6289d373fcfaa64b09e6c63a43973
willstreet <william.alexander13@stjohns.edu>
2017-04-25 00:29:28 -0400

    business

--- js/Will.js
+++ js/Will.js
@@ -79,7 +79,7 @@ function create() {
     //ledge.body.immovable = true;
 
     // The player and its settings
-    player1 = game.add.sprite(32, game.world.height - 150, 'thief');
+    player1 = game.add.sprite(32, game.world.height - 150, 'business');
 	//player2 = game.add.sprite(32, game.world.height - 150, 'thief');
 	//player1.frame = 1;
 	//player2.frame = 1;
@@ -94,8 +94,8 @@ function create() {
     player1.body.collideWorldBounds = true;
 
      //  Our two animations, walking left and right.
-	//player1.animations.add('left', [3, 4, 5], 5, true);
-	//player1.animations.add('right', [0, 1, 2], 5, true); 
+	player1.animations.add('left', [4, 5, 6, 7], 7, true);
+	player1.animations.add('right', [0, 1, 2, 3], 7, true); 
 	
 	/* //  Player2 physics properties. Give the little guy a slight bounce.
     player2.body.bounce.y = 0.2;
@@ -198,7 +198,7 @@ function update() {
         //  Move to the left
         player1.body.velocity.x = -150;
 
-       // player1.animations.play('left');
+        player1.animations.play('left');
 		//school.tilePosition.x+= 5;
     }
     else if (cursors.right.isDown)
@@ -206,7 +206,7 @@ function update() {
         //  Move to the right
         player1.body.velocity.x = 150;
 
-       // player1.animations.play('right');
+       player1.animations.play('right');
 		//school.tilePosition.x-=5;
     }
     else
