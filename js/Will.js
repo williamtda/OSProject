8f8532f5fe094aef887855d10942d32a99771ee8
willstreet <william.alexander13@stjohns.edu>
2017-04-24 23:38:23 -0400

    player1

--- js/Will.js
+++ js/Will.js
@@ -262,7 +262,7 @@ function updatePlayer() {
     }
     
     //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
+    if (cursors.up.isDown && player1.body.touching.down)
     {
         player1.body.velocity.y = -350;
     }
