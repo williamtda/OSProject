b3bb7a49ecdb7f49e3a947b083318cca0e28e5c2
willstreet <william.alexander13@stjohns.edu>
2017-04-26 14:40:10 -0400

    cleanup

--- js/Will.js
+++ js/Will.js
@@ -13,8 +13,7 @@ function preload() {
 	 game.load.image('ground', 'assets/platform.png');
 	// game.load.image('menu', 'assets/blackbox.png', 360, 200);
 	 
-	//add sound
-	//game.load.audio('music', 'assets/audio/How_It_Began.mp3'); 
+
 
 }
 
@@ -29,15 +28,11 @@ var Akey;
 var Wkey;
 var Skey;
 var Dkey;
-var homework;
 var balance = 0;
 var balanceText;
 
-//CHANGE
-var aTest;
 
-//add sound
-var sound;
+
 
 //Timer
 var timer;
@@ -45,13 +40,7 @@ var timeLeft = 32;
 var timerText = 0;
 
 function create() {
-	//superKey = game.input.keyboard.addKey(Phaser.Keyboard.Z);
-
-    //add sound
-	//game.input.touch.preventDefault = false;
-	//sound = game.add.audio('music');
-	//sound.play();
-	//game.input.onDown.add(restartMusic, this);
+	
 	
 	//  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -129,7 +118,7 @@ function create() {
 
 
     //  The score
-    //scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    balanceText = game.add.text(16, 16, 'balance: 0', { fontSize: '32px', fill: '#000' });
 	
 	//The Time left
 	//timerText = game.add.text(670, 13, 'Time: 32', { fontSize: '32px', fill: '#000' });
@@ -145,45 +134,10 @@ function create() {
 	Skey = game.input.keyboard.addKey(Phaser.Keyboard.S);
 	Dkey = game.input.keyboard.addKey(Phaser.Keyboard.D);
 	
-	//  The first parameter is how long to wait before the event fires. In this case 5 seconds (you could pass in 2000 as the value as well.)
-    //  The second parameter is how many times the event will run in total. Here we'll run it 2 times.
-    //  The next two parameters are the function to call ('createBall') and the context under which that will happen.
-
-    //  Once the event has been called 2 times it will never be called again.
-
-    //game.time.events.repeat(Phaser.Timer.SECOND * 5, 2, createHomework, this);
-	
-	//  AT 15 SECOND MARK
-	//  Here we'll create a basic timed event. This is a one-off event, it won't repeat or loop:
-    //  The first parameter is how long to wait before the event fires. In this case 15 seconds 
-    //  The next parameter is the function to call ('halfTime') and finally the context under which that will happen.
-
-    //game.time.events.add(Phaser.Timer.SECOND * 15, halfTime, this);
-	
-	//  AT 30 SECONDS
-	//game.time.events.add(Phaser.Timer.SECOND * 30, createTest, this);
-	
-	//  AT 33 SECONDS
-	//game.time.events.add(Phaser.Timer.SECOND * 33, endGame, this); // Testing purposes only
 }
 
-/* function returnGrade(score){
-	if (score >= 90){
-		return "A";
-	} else if (score >= 80){
-		return "B";
-	} else if (score >= 70){
-		return "C";
-	} else if (score >= 60){
-		return "D";
-	} else {
-		return "F";
-	}
-} */
 
-/* function restartMusic() {
-	sound.restart();
-}
+
 
 function updateTimer() {
 
@@ -278,43 +232,7 @@ function update() {
     }
 }
 
-/* function updateSuperPlayer() {
-	player1.body.velocity.x = 0;
-	player2.body.velocity.x = 0;
-	if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player1.body.velocity.x = -250;
 
-        player1.animations.play('left');
-		//school.tilePosition.x+= 5;
-    }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player1.body.velocity.x = 250;
-
-        player1.animations.play('right');
-		//school.tilePosition.x-=5;
-    }
-    else
-    {
-        //  Stand still
-        player1.animations.stop();
-
-        player1.frame = 4;
-    }
-    
-    //  Allow the player to jump
-    if (cursors.up.isDown)
-    {
-        player1.body.velocity.y = -300;
-    }
-	else if (cursors.down.isDown)
-	{
-		player1.body.velocity.y += 300;
-	}
-} */
 
 /* function updatePlayer() {
 	//  Reset the players velocity (movement)
@@ -324,62 +242,7 @@ function update() {
 } */
 
 
-/* function createHomework() {
-	try {
-		homework.kill();
-	} catch (err){
-		
-	}
-	
-	var homeworkFall = Math.random()*10*70 + 1; // Falls between 70 and width - 70 px
-	// The player and its settings
-    homework = game.add.sprite(homeworkFall, 0, 'homework');
-	homework.points = 15;
 
-    //  We need to enable physics on the homework
-    game.physics.arcade.enable(homework);
-	
-	homework.body.gravity.y = 300;
-
-} */
-
-/* function createTest() {
-	try {
-		aTest.kill();
-		homework.kill();
-	} catch (err){
-		
-	}
- */
-	/* var testFall = Math.random()*10*70 + 1; // Falls between 70 and width - 70 px
-	The player and its settings
-    aTest = game.add.sprite(testFall, 0, 'test');
-
-     We need to enable physics on the test
-    game.physics.arcade.enable(aTest);
-	
-	aTest.body.gravity.y = 300; */
-	
-	/*
-	//  Finally some tests to collect
-    tests = game.add.group();
-
-    //  We will enable physics for any test that is created in this group
-    tests.enableBody = true;
-	
-	var testFall = Math.random()*10*70 + 1; // Falls between 70 and width - 70 px
-	
-	var aTest = tests.create(testFall,0,'test');
-	aTest.body.gravity.y = 100; // TODO: make it fall slower
-	*/
-
-
-
-/* function halfTime(homework){
-	
-	createTest();
-	game.time.events.repeat(Phaser.Timer.SECOND * 5, 2, createHomework, this);
-} */
 
 /* function render() 
 {
@@ -393,58 +256,8 @@ function update() {
 
 } */
 
-/* function collectHomework (player, homework) {
-    
-    // Removes the homework from the screen
-    homework.kill();
 
-    //  Add and update the score
-    score += homework.points;
-    scoreText.text = 'Score: ' + score;
 
-} */
 
-/* function collectTest (player, aTest) {
-    
-    // Removes the homework from the screen
-    aTest.kill();
 
-    //  Add and update the score
-    score += 20;
-    scoreText.text = 'Score: ' + score;
 
-} */
-
-/* function endGame() {
-	// When the pause button is pressed, we pause the game
-    game.paused = true;
-	var w = game.world.width;
-	var h = game.world.height;
-
-	// Then add the menu
-	var menu = game.add.sprite(w/2, h/2, 'menu');
-	menu.anchor.setTo(0.5, 0.5);
-	var endMessage = "GRADE: "+returnGrade(score);
-	if (score < 60){
-		endMessage = endMessage+ "\nYOU FAILED!"; 
-	}
-	if (score == 100){
-		endMessage = endMessage+ "\nAMAZING!";
-	}
-	else{
-		endMessage = endMessage+ "\nTHANKS FOR PLAYING!";
-	}
-	var endText = game.add.text(game.world.centerX, game.world.centerY, endMessage,{fill: '#fff' });
-	endText.anchor.setTo(0.5,0.5);
-
-	// And a label to illustrate which menu item was chosen. (This is not necessary)
-	var choiseLabel = game.add.text(game.world.centerX, game.world.centerY + menu.height/2+30, 'Click here to restart', {fill: '#000000' });
-	choiseLabel.anchor.setTo(0.5, 0.5);
-
-	
-	// Add a input listener that can help us return from being paused
-    game.input.onDown.add(restart, self);
-	function restart(event){
-		location.reload();
-	}
-} */
